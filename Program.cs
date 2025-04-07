9a25a7a097b2a4a1b1e6af51b7847c4d99f789b2
Kawan Weber <kawan_weber@hotmail.com>
2025-04-06 22:14:00 -0300

    Att FGTS

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Cálculo do FGTS");
+Console.WriteLine("Informe o salário: ");
+string? input = Console.ReadLine();
+
+decimal salário = Convert.ToDecimal(input);
+decimal FGTS = salário * 0.08m;
+
+Console.WriteLine($"FGTS: R$ {FGTS:F2}");
